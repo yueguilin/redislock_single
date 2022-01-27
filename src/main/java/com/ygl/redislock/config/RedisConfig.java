01e556aad8c10076b3a3dc80593b9d7a6a379530
ygl <guiliny@aliyun.com>
2022-01-27 14:35:45 +0800

    测试完成

---
+++ src/main/java/com/ygl/redislock/config/RedisConfig.java
@@ -0,0 +1,32 @@
+package com.ygl.redislock.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.io.Serializable;
+
+/**
+ * @auther zzyy
+ * @create 2020-09-15 14:47
+ */
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Serializable> redisTemplate(LettuceConnectionFactory connectionFactory)
+    {
+        RedisTemplate<String, Serializable> redisTemplate = new RedisTemplate<>();
+
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setConnectionFactory(connectionFactory);
+
+        return redisTemplate;
+    }
+}
+
+
